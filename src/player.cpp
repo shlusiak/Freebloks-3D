3e171baf69446fa6fbc7e0b75abff0549a014425
Sascha Hlusiak <mail@saschahlusiak.de>
2008-10-17 18:14:28 +0200

    Remove number_of_denied_fields

--- src/player.cpp
+++ src/player.cpp
@@ -40,11 +40,25 @@ void CPlayer::refresh_data(const CSpiel* spiel){
 	CPlayer::m_number_of_possible_turns = 0;
 	CPlayer::m_position_points = 0;
 	CPlayer::m_stone_count = 0;
+	CPlayer::m_number_of_allowed_fields = 0;	
 
 	for (int n = 0; n < STONE_COUNT_ALL_SHAPES; n++){
 		CStone* stone = &CPlayer::m_stone[n];
-		CPlayer::m_stone_count += stone->get_available();
-		CPlayer::m_stone_points_left += stone->get_stone_points() * stone->get_available();
+		if (stone->get_available()){
+			int pos_turns = 0;
+			CPlayer::m_stone_count += stone->get_available();
+			CPlayer::m_stone_points_left += stone->get_stone_points() * stone->get_available();
+
+			for (int x = 0; x < spiel->get_field_size_x(); x++){
+				for (int y = 0; y < spiel->get_field_size_y(); y++){
+					if (spiel->get_game_field(CPlayer::m_number, y, x) == FIELD_ALLOWED){
+						pos_turns += stone->calculate_possible_turns_in_position(spiel, CPlayer::m_number, y, x);
+					}
+				}	
+			}	
+			CPlayer::m_number_of_possible_turns += pos_turns;
+			CPlayer::m_position_points += pos_turns * stone->get_stone_position_points() * stone->get_stone_points(); //ist ein guter wert!!!!
+		}
 	}
 
 	for (int x = 0; x < spiel->get_field_size_x(); x++){
--- src/player.h
+++ src/player.h
@@ -18,6 +18,7 @@ class CPlayer{
 		int m_stone_count;
 		int m_number_of_possible_turns;
 		int m_position_points;
+		int m_number_of_allowed_fields;
 
 		int m_teammate;
 		int m_nemesis;
@@ -36,6 +37,7 @@ class CPlayer{
 		const int get_stone_points_left()const;
 		const int get_position_points()const; //gibt eine situationsbewertung zur�ck
 		const int get_number_of_possible_turns()const;
+		const int get_number_of_allowed_fields()const;
 		const int get_stone_count()const;
 		
 		const int get_teammate()const;
@@ -62,6 +64,10 @@ CStone* CPlayer::get_stone(const int stonenumber){
 	return &CPlayer::m_stone[stonenumber];
 }
 
+inline
+const int CPlayer::get_number_of_allowed_fields()const{
+	return CPlayer::m_number_of_allowed_fields;
+}
 
 inline
 const int CPlayer::get_number_of_possible_turns()const{
